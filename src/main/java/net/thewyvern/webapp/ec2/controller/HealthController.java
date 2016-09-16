687efc59cbe390ef2d6ba1d1bb0767b556ecbfed
Matt Devaney <matthew.devaney@worldpay.com>
2016-09-16 11:06:58 +0100

    sycning my local tree with Marcus' clean-up; adding context.xml back

---
+++ src/main/java/net/thewyvern/webapp/ec2/controller/HealthController.java
@@ -0,0 +1,45 @@
+package net.thewyvern.webapp.ec2.controller;
+
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HealthController extends BaseController {
+
+	/**
+	 * Checks to see if we're able to create new HTTP Session. If we are, just return status 200, otherwise 503.
+	 * 
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/health", method = RequestMethod.GET)
+	public ResponseEntity<String> getApplicationStatus(HttpServletRequest request) {
+
+		String now = LocalDateTime.now().toString();
+		String logMessage;
+		String responseMessage;
+		HttpStatus responseStatus;
+
+		HttpSession session = request.getSession(true);
+		if (session == null) {
+			logMessage = "NGPP service not available at: " + now;
+			responseMessage = logMessage;
+			responseStatus = HttpStatus.SERVICE_UNAVAILABLE;
+		} else {
+			session.invalidate();
+			logMessage = "NGPP Status OK as of: " + now;
+			responseMessage = logMessage;
+			responseStatus = HttpStatus.OK;
+		}
+		return new ResponseEntity<>(responseMessage, responseStatus);
+	}
+
+}

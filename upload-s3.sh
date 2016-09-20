6916d609b6a1bf09cfcc2ee6a6df071ff51d0b11
limpygnome <limpygnome@gmail.com>
2016-09-20 23:52:42 +0100

    Cleaning up S3 auto-deployment.

--- upload-s3.sh
+++ upload-s3.sh
@@ -1,4 +1,4 @@
-# Uploads to bucket called "testapp-global"
+# Uploads to bucket called "testapp-global" manually
+# -- Requires s3cmd; sudo apt-get install s3cmd on Ubuntu at least, and then s3cmd --configure
 
-s3cmd put -r /home/limpygnome/git-remote/ec2-simple-test/target/SimpleEC2App-0.0.1-SNAPSHOT/resources/content/ s3://testapp-global/content/ --acl-public
-
+s3cmd put -r target/SimpleEC2App-0.0.1-SNAPSHOT/resources/content/ s3://testapp-global/content/ --acl-public
